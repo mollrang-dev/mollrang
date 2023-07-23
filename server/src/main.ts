a00c4a83809651b263ed3f44118579ad338fde88
2Ruk <leerukdev@gmail.com>
2023-07-23 23:46:30 +0900

    feat[#4]: cors setting

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,7 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('/api')
+  app.setGlobalPrefix('/api');
   await app.listen(3000);
+
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    credentials: true,
+  });
 }
 bootstrap().then();
