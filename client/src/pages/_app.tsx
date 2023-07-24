e3449b33b2c6791b26e8a849f01ca4afccc2d828
dahoon06 <dahoon226@naver.com>
2023-07-24 18:49:08 +0900

    feat (#5, #19): react-query setup 작업

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,16 +1,37 @@
 import {LayoutComponent} from "@components/layout/LayoutComponent";
-import type {AppProps} from "next/app";
+import type {AppProps, AppContext, AppInitialProps} from "next/app";
 import "@styles/_reset.scss";
 import ErrorBoundary from "@utils/error/errorBoundary";
+import {QueryClientProvider, Hydrate} from "@tanstack/react-query";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
+import {queryClient} from "@libs/tanstack";
+import {NextComponentType} from "next";
+import React from "react";
+import {dehydrate} from '@tanstack/react-query'
 
-export default function App({Component, pageProps}: AppProps) {
+const App: NextComponentType<AppContext, AppInitialProps, AppProps> = ({Component, pageProps}) => {
+  const queryClientRef = React.useRef<queryClient>();
+
   return (
     <>
       <ErrorBoundary>
-        <LayoutComponent>
-          <Component {...pageProps} />
-        </LayoutComponent>
+        <QueryClientProvider client={queryClient}>
+          <LayoutComponent>
+            <Component {...pageProps} />
+          </LayoutComponent>
+          <ReactQueryDevtools initialIsOpen={false}/>
+        </QueryClientProvider>
       </ErrorBoundary>
     </>
   );
 }
+App.getInitialProps = async ({Component, ctx}: AppContext): Promise<AppInitialProps> => {
+  let pageProps = {}
+
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx)
+  }
+
+  return {pageProps}
+}
+export default App;
