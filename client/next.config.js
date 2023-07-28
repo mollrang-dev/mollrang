a0573eea39bd2ecfb2831d4f3ba59d838ee31cfd
dahoon06 <dahoon226@naver.com>
2023-07-28 13:55:32 +0900

    fix: next parsing error
    error message: Cannot find module 'next/babel'
    
    # solution
    
    root 위치에 babelrc 생성 -> next/babel 추가
    
    eslint extend 에 babel/next 추가

--- client/next.config.js
+++ client/next.config.js
@@ -1,67 +1,79 @@
 /** @type {import('next').NextConfig} */
-const path = require('path');
+const path = require("path");
 
 module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: "/",
+        destination: "/",
+      },
+      {
+        source: "/quizzes/:quizId",
+        destination: "/quizes/:quizId",
+      },
+    ];
+  },
   images: {
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-    domains: ['img.shields.io', 'picsum.photos'],
+    domains: ["img.shields.io", "picsum.photos"],
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'img.shields.io',
-        port: '',
-        pathname: '/badge/**',
+        protocol: "https",
+        hostname: "img.shields.io",
+        port: "",
+        pathname: "/badge/**",
       },
       {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/400/**',
+        protocol: "https",
+        hostname: "picsum.photos",
+        port: "",
+        pathname: "/400/**",
       },
     ],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    path: '/_next/image',
-    loader: 'default',
+    path: "/_next/image",
+    loader: "default",
     disableStaticImages: false,
     minimumCacheTTL: 60,
-    formats: ['image/webp'],
+    formats: ["image/webp"],
   },
   reactStrictMode: true,
   typescript: {},
   serverRuntimeConfig: {
     PROJECT_ROOT: __dirname,
   },
-  distDir: '.next',
+  distDir: ".next",
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
-      use: ['@svgr/webpack'],
+      use: ["@svgr/webpack"],
     });
     return config;
   },
   sassOptions: {
-    includePaths: [path.join(__dirname, 'src/styles')],
+    includePaths: [path.join(__dirname, "src/styles")],
     prependData: `
       @import "./_color.scss";
        @import "./_utils.scss";
       `,
   },
   moduleNameMapper: {
-    '^@pages/(.*)$': '<rootDir>/src/pages/$1',
-    '^@components/(.*)$': '<rootDir>/src/components/$1',
-    '^@api/(.*)$': '<rootDir>/api/$1',
-    '^@apis/(.*)$': '<rootDir>/apis/$1',
-    '^@constants/(.*)$': '<rootDir>/constants/$1',
-    '^@libs/(.*)$': '<rootDir>/libs/$1',
-    '^@store/(.*)$': '<rootDir>/store/$1',
-    '^@utils/(.*)$': '<rootDir>/utils/$1',
-    '^@hooks/(.*)$': '<rootDir>/hooks/$1',
-    '^@styles/(.*)$': '<rootDir>/styles/$1',
-    '^@interfaces/(.*)$': '<rootDir>/interfaces/$1',
-    '^@images/(.*)$': '<rootDir>/images/$1',
-    '^@assets/(.*)$': '<rootDir>/assets/$1',
+    "^@pages/(.*)$": "<rootDir>/src/pages/$1",
+    "^@components/(.*)$": "<rootDir>/src/components/$1",
+    "^@api/(.*)$": "<rootDir>/api/$1",
+    "^@apis/(.*)$": "<rootDir>/apis/$1",
+    "^@constants/(.*)$": "<rootDir>/constants/$1",
+    "^@libs/(.*)$": "<rootDir>/libs/$1",
+    "^@store/(.*)$": "<rootDir>/store/$1",
+    "^@utils/(.*)$": "<rootDir>/utils/$1",
+    "^@hooks/(.*)$": "<rootDir>/hooks/$1",
+    "^@styles/(.*)$": "<rootDir>/styles/$1",
+    "^@interfaces/(.*)$": "<rootDir>/interfaces/$1",
+    "^@images/(.*)$": "<rootDir>/images/$1",
+    "^@assets/(.*)$": "<rootDir>/assets/$1",
   },
   trailingSlash: true,
   swcMinify: true,
