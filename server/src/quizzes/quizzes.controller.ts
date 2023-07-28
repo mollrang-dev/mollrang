29c773355613f9f41f4207246835943a8b8850a7
2Ruk <leerukdev@gmail.com>
2023-07-28 17:11:41 +0900

    feat[#17]: quizzes controller swagger
    - response, input dto에 대해서는 작업이 필요한 상황입니다.
    - 현재 기본적인 query, params 에 대한 입력값을 명시했습니다.

--- server/src/quizzes/quizzes.controller.ts
+++ server/src/quizzes/quizzes.controller.ts
@@ -1,34 +1,141 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Param } from '@nestjs/common';
 import { QuizzesService } from './quizzes.service';
-import { CreateQuizDto } from './dto/create-quiz.dto';
-import { UpdateQuizDto } from './dto/update-quiz.dto';
+import { ApiOperation, ApiParam, ApiQuery, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('quizzes')
 @Controller('quizzes')
 export class QuizzesController {
   constructor(private readonly quizzesService: QuizzesService) {}
 
-  @Post()
-  create(@Body() createQuizDto: CreateQuizDto) {
-    return this.quizzesService.create(createQuizDto);
-  }
-
+  // TODO: INPUT DTO, RESPONSE DTO 정의
+  /**
+   * GET /quizzes
+   * query: size
+   * 퀴즈 리스트 조회 ( size 개수 만큼 List 반환 )
+   */
+  @ApiOperation({ summary: '퀴즈 리스트 조회' })
+  @ApiQuery({
+    name: 'size',
+    type: Number,
+    description: '퀴즈 리스트 개수',
+  })
   @Get()
   findAll() {
     return this.quizzesService.findAll();
   }
 
-  @Get(':id')
+  // TODO: INPUT DTO, RESPONSE DTO 정의
+  // TODO: middleware 를 통해 userId를 가져올 것, Guard 로직 추가가 필요함
+  /**
+   * GET /quizzes/correct/:userId
+   * @param userId
+   * @description 유저가 맞춘 퀴즈 리스트 조회
+   */
+  @ApiOperation({ summary: '유저가 맞춘 퀴즈 리스트 조회' })
+  @ApiQuery({
+    name: 'currentPage',
+    type: Number,
+    description: '페이지 번호',
+  })
+  @ApiQuery({
+    name: 'limit',
+    type: Number,
+    description: '페이지당 퀴즈 개수',
+  })
+  @ApiParam({
+    name: 'userId',
+    type: String,
+  })
+  @Get('correct/:userId')
+  findCorrectQuizList(@Param('userId') userId: string) {
+    return {
+      result: true,
+    };
+  }
+
+  // TODO: INPUT DTO, RESPONSE DTO 정의
+  // TODO: middleware 를 통해 userId를 가져올 것, Guard 로직 추가가 필요함
+  /**
+   * GET /quizzes/incorrect/:userId
+   * @param userId
+   * @description 유저가 틀린 퀴즈 리스트 조회
+   */
+  @ApiOperation({ summary: '유저가 틀린 퀴즈 리스트 조회' })
+  @ApiQuery({
+    name: 'currentPage',
+    type: Number,
+    description: '페이지 번호',
+  })
+  @ApiQuery({
+    name: 'limit',
+    type: Number,
+    description: '페이지당 퀴즈 개수',
+  })
+  @ApiParam({
+    name: 'userId',
+    type: String,
+  })
+  @Get('incorrect/:userId')
+  findIncorrectQuizList(@Param('userId') userId: string) {
+    return {
+      result: true,
+    };
+  }
+
+  // TODO: INPUT DTO, RESPONSE DTO 정의
+  /**
+   * GET /quizzes/:id
+   * @param id
+   * @description 퀴즈 단일 조회
+   */
+  @ApiOperation({ summary: '퀴즈 단일 조회' })
+  @ApiParam({
+    name: 'quizId',
+    type: String,
+    description: '퀴즈 아이디',
+  })
+  @Get(':quizId')
   findOne(@Param('id') id: string) {
     return this.quizzesService.findOne(+id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateQuizDto: UpdateQuizDto) {
-    return this.quizzesService.update(+id, updateQuizDto);
+  // TODO: middleware 를 통해 userId를 가져올 것, Guard 로직 추가가 필요함
+  /**
+   * POST /quizzes/:quizId/favorite
+   * @param quizId
+   * @description 퀴즈 즐겨찾기 추가
+   */
+  @ApiOperation({ summary: '퀴즈 즐겨찾기 추가' })
+  @ApiParam({
+    name: 'quizId',
+    type: String,
+    description: '퀴즈 아이디',
+  })
+  @Post(':quizId/favorite')
+  createFavorite(@Param('quizId') quizId: string) {
+    return {
+      result: true,
+    };
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.quizzesService.remove(+id);
+  // TODO: middleware 를 통해 userId를 가져올 것, Guard 로직 추가가 필요함
+  // TODO: 추후 Controller 분리 고민 필요
+  /**
+   * POST /quizzes/:quizId/answer
+   * @param quizId
+   * @description 유저의 정답 제출
+   */
+  @ApiTags('quizzes-answer')
+  @ApiOperation({ summary: '유저의 정답 제출' })
+  @ApiParam({
+    name: 'quizId',
+    type: String,
+    description: '퀴즈 아이디',
+  })
+  @Post(':quizId/answer')
+  createAnswer(@Param('quizId') quizId: string) {
+    return {
+      result: true,
+    };
   }
 }
