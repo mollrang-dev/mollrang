1dfe240c237b8bf8ef867d646c1b623e645c33c4
dahoon06 <dahoon226@naver.com>
2023-07-28 13:53:53 +0900

    feat (#28): 퀴즈 폼 UI 작업

--- client/src/components/domain/quizzes/question-form/QuestionForm.tsx
+++ client/src/components/domain/quizzes/question-form/QuestionForm.tsx
@@ -1,14 +1,23 @@
-import {FormUi} from "@components/ui/form/FormUi";
-import {ReactElement} from "react";
-import styles from './QuestionForm.module.scss';
+import { FormUi } from "@components/ui/form/FormUi";
+import { ReactElement } from "react";
+import styles from "./QuestionForm.module.scss";
+import { Typography } from "@components/common/typography/Typography";
 
 export const QuestionForm = (): ReactElement => {
   return (
     <div className={styles.question_form}>
       <FormUi>
-        Q1
-        body
+        <div className={styles.question_label}>
+          <Typography color="white" variant="body2" weight="bold">
+            Q1
+          </Typography>
+        </div>
+        <div className={styles.question_body}>
+          <Typography color="gray100" variant="body2" weight="bold">
+            Node.js TESt
+          </Typography>
+        </div>
       </FormUi>
     </div>
-  )
-}
+  );
+};
